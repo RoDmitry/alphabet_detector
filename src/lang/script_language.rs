64d630790e5df4ce66991cc2a53595e1ad5f2c2a
RoDmitry <gh@rdmtr.com>
2025-04-21 18:14:51 +0000

    Docs

--- src/lang/script_language.rs
+++ src/lang/script_language.rs
@@ -8,8 +8,8 @@ use strum_macros::{EnumCount as EnumCountMacro, EnumIter};
 const ENUM_NAME: &str = "ScriptLanguage";
 
 /// Language + script.
-/// Value-names not always represent a script used, so a "default" script can be changed,
-/// parts representation (const
+/// Value-names not always represent a script used, so a "default" script can be changed.
+/// Parts representation (const
 /// [`into_parts`](enum.ScriptLanguage.html#method.into_parts)/[`from_parts`](enum.ScriptLanguage.html#method.from_parts))
 /// or string representation (const
 /// [`into_str`](enum.ScriptLanguage.html#method.into_str)/[`from_str`](enum.ScriptLanguage.html#method.from_str))
