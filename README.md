c08db266e8860b43194c4e5f01f76aed7af80bf3
RoDmitry <gh@rdmtr.com>
2025-05-28 12:19:02 +0000

    Release 0.8.0

--- README.md
+++ README.md
@@ -12,22 +12,6 @@ It's more like a word separator + language prefilter for an actual language dete
 
 Splits text (iterator `CharIndices`) to words, and detects [`ScriptLanguage`](https://docs.rs/alphabet_detector/latest/alphabet_detector/enum.ScriptLanguage.html)s (language + script) of words by used letters (chars).
 
-### Examples
-
-To split `text` to the iterator of [`Word`](https://docs.rs/alphabet_detector/latest/alphabet_detector/words/struct.Word.html):
-```rust
-let word_iter = words::from_ch_ind::<Vec<char>>(text.char_indices());
-```
-
-If you don't need individual words, but just want to analyze a full text:
-```rust
-let (all_words, all_langs) = fulltext_filter_with_margin_sorted::<Vec<char>, 95>(text.char_indices());
-```
-
-It will give you all [`Word`](https://docs.rs/alphabet_detector/latest/alphabet_detector/struct.Word.html)s (`Vec<Word<Vec<char>>>`) of `text` and `Vec<(ScriptLanguage, u32)>` filtered with a less then 5% margin for an error.
-
-Instead of `Vec<char>` you can use [other types](https://docs.rs/alphabet_detector/latest/alphabet_detector/words/trait.WordBuf.html#foreign-impls) of words.
-
 ### Extras
 
 Look at the [alphabets.rs](https://github.com/RoDmitry/alphabet_detector/blob/main/src/lang/alphabets.rs#L73) to understand what languages have already defined alphabets. Some of them need validation.
--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,32 @@
 //!
 //! Does not have any models, just matches the alphabet. Not recommended to use as a standalone detector.
 //! It's more like a word separator + language prefilter for an actual language detector ([`Langram`](https://docs.rs/langram/latest/langram)).
+//!
 //! Splits text (iterator `CharIndices`) to words, and detects [`ScriptLanguage`](enum.ScriptLanguage.html)s (language + script) of words by used letters (chars).
 //!
 //! `ISO 639-3` (using [`Language`](enum.Language.html#implementations)) and `ISO 15924` (using [`Script`](enum.Script.html#implementations))
 //! are implemented, also combined using [`ScriptLanguage`](enum.ScriptLanguage.html#implementations).
+//!
+//! # Examples
+//! To split `text` to the iterator of [`Word`](words/struct.Word.html):
+//! ```rust
+//! use alphabet_detector::words;
+//!
+//! let text = "test text";
+//! let word_iter = words::from_ch_ind::<Vec<char>>(text.char_indices());
+//! ```
+//!
+//! If you don't need individual words, but just want to analyze a full text:
+//! ```rust
+//! use alphabet_detector::fulltext_filter_with_margin_sorted;
+//!
+//! let text = "test text";
+//! let (all_words, all_langs, _) = fulltext_filter_with_margin_sorted::<Vec<char>, 95>(text.char_indices());
+//! ```
+//!
+//! It will give you all [`Word`](struct.Word.html)s (`Vec<Word<Vec<char>>>`) of `text` and `Vec<(ScriptLanguage, u32)>` filtered with a less then 5% margin for an error.
+//! 
+//! Instead of `Vec<char>` you can use [other types](words/trait.WordBuf.html#foreign-impls) of words.
 
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![cfg_attr(feature = "files_read", feature(string_into_chars))]
